547a63b51a57cdaa7691fa1304dae83e7bf99e85
Jiyu Tian <tjyiiuan@gmail.com>
2018-06-06 20:35:34 -0400

    update dissimilar

--- test/testsimilar.py
+++ test/testsimilar.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import scipy.stats
 import numpy as np
 from libs.process import detect_onset
@@ -9,14 +8,29 @@ import matplotlib.pylab as plt
 from matplotlib.pylab import rcParams
 rcParams['figure.figsize'] = 15, 6
 
-def dissimilar(seir1, seir2):
+def dissimilar(twosier, slot = 50, show = True):
+    '''
+    Compare each part of two time series data with a fixed sliding window, 
+    and those below the overall Pearson correlation coefficient are detected 
+    as dissimilar.
+    
+    Parameters
+    ----------       
+    twosier : two 1-D array-like
+         Time series data to compare.
+    Returns
+    -------
+    ind : 1-D array-like
+        Indices of dissimilar parts.
+
+    '''
+    seir1, seir2 = twosier
     assert len(seir1) == len(seir2)
     
     nsample = len(seir1)
-    num = 50
     
-    sp1 = [seir1[i: i + num] for i in np.arange(nsample - num + 1)]
-    sp2 = [seir2[i: i + num] for i in np.arange(nsample - num + 1)]
+    sp1 = [seir1[i: i + slot] for i in np.arange(nsample - slot + 1)]
+    sp2 = [seir2[i: i + slot] for i in np.arange(nsample - slot + 1)]
     
     res = []
     for i in np.arange(len(sp1)):
@@ -24,4 +38,27 @@ def dissimilar(seir1, seir2):
     
     ind = detect_onset(-np.array(res)[:, 0], n_above = 5,
                        threshold = -scipy.stats.pearsonr(seir1, seir2)[0])   
+    
+#    if len(ind):
+#        for tmpind in ind:
+#            if np.diff(tmpind) >= slot:
+#                tmpind[0] += slot
+#        tmpind[-1][1] == nsample - slot
+        
+    if show:
+        plt.figure()
+        plt.subplot(221)
+        plt.plot(seir1)
+        plt.subplot(222)
+        plt.plot(seir2)
+        plt.subplot(223)
+        plt.plot(1-np.array(res)[:, 0])
+        plt.subplot(224)
+        plt.plot(seir1)
+        plt.plot(seir2)
+        for oneind in ind:
+            plt.axvline(x=oneind[0], color='b', lw=1, ls='--')
+            plt.axvline(x=oneind[1], color='b', lw=1, ls='--')
+            
     return ind
+
